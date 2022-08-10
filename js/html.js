a863531ed7d1b85be2dd7dee242e0f35b623c330
ElitistNoob <89539295+ElitistNoob@users.noreply.github.com>
2022-08-10 12:06:29 +0700

    Update html.js

--- js/html.js
+++ js/html.js
@@ -25,8 +25,8 @@ function renderMovies(data) {
 
 function renderAddBtn(data) {
     const movieIndex = storedMovies.findIndex(id => id === data.imdbID)
-    const url = '/elitistnoob.github.io/scrimba-watchlist-project/index.html'
-    if(location.pathname === url) {
+    if(location.pathname === '/scrimba-watchlist-project/index.html' 
+      || location.pathname === '/scrimba-watchlist-project/') {
         if(movieIndex === -1) {
             return `<p><span class="iconify" data-icon="ant-design:plus-circle-filled" style="color: white;" data-width="16"></span> Watchlist</p>`
         } else {
